b2d252f098930713ab17866dd6f4f6307ca98cdf
Deniz451 <114061026+Deniz451@users.noreply.github.com>
2024-03-19 22:20:26 +0100

    Commit for school - 03.20.
    backup for working in school on 03.20.

--- script.js
+++ script.js
@@ -1,73 +1,105 @@
-// Navbar opening function
+// Event listener for color mode change
+const colorModeButton = document.getElementById('colorMode');
+colorModeButton.addEventListener('click', toggleColorMode);
+
+// Event listener for language change
+const languageButton = document.getElementById('language');
+languageButton.addEventListener('click', changeLanguage);
+
+// Event listener for navbar
+const navOpenButton = document.getElementById('navOpenButton');
+navOpenButton.addEventListener('click', openNav);
+const navCloseButton = document.getElementById('navCloseButton');
+navCloseButton.addEventListener('click', closeNav);
+
+// show cards eventlistener
+// hide cards eventlistener
+ 
+
+
+// Navbar functions
 function openNav() {
   document.getElementById("navbar").style.width = "250px";
   }
-  
+
 function closeNav() {
   document.getElementById("navbar").style.width = "0";
 }
 
-
-
-
-// Functions for showing about card
+// Showing/hiding cards functions
 function showAboutCard() {
   var card = document.getElementById('card-about');
   card.style.display = 'flex';
   card.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
-
-// Functions for hiding about card
 function hideAboutCard() {
   var card = document.getElementById('card-about');
+  var header = document.getElementById('about');
   card.style.display = 'none';
-  window.scrollTo({ top: 700, behavior: 'smooth' });
+  header.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
-
-
-// Functions for showing coding card
 function showCodingCard() {
   var card = document.getElementById('card-coding');
   card.style.display = 'flex';
   card.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
-
-
-// Functions for hiding coding card
 function hideCodingCard() {
   var card = document.getElementById('card-coding');
+  var header = document.getElementById('coding');
   card.style.display = 'none';
-  window.scrollTo({ top: 1400, behavior: 'smooth' });
+  header.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
-
-
-// Functions for showing 3D modeling card
 function showModelingCard() {
   var card = document.getElementById('card-modeling');
   card.style.display = 'flex';
   card.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
-
-// Functions for hiding 3D modeling card
 function hideModelingCard() {
   var card = document.getElementById('card-modeling');
+  var header = document.getElementById('modeling');
   card.style.display = 'none';
-  window.scrollTo({ top: 2100, behavior: 'smooth' });
+  header.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
 
+// Function for color mode change
+function toggleColorMode() {
+  var root = document.documentElement;
 
+  const computedStyle = getComputedStyle(colorModeButton);
+  const color = computedStyle.getPropertyValue('color');
+  
 
+  if(computedStyle.getPropertyValue('color') == 'rgb(0, 0, 0)'){
+    root.style.setProperty('--bg-light1', 'rgb(0, 0, 0)');
+    root.style.setProperty('--bg-light2', 'rgb(25, 25, 25)');
+    
+    root.style.setProperty('--icon-color', 'rgb(255, 255, 255)');
 
-// Function for ligh mode change
-function colorModeChange() {
+    root.style.setProperty('--header-color', 'rgb(255, 255, 255)');
 
+    root.style.setProperty('--footer-color', 'rgb(255, 255, 255)');
+
+    root.style.setProperty('--navbar-bg-color', 'rgb(255, 255, 255)');
+    root.style.setProperty('--navbar-color', 'rgb(0, 0, 0)');
+    root.style.setProperty('--navbar-color-hover', 'rgb(100, 100, 100)');
+  }
+  else{
+    root.style.setProperty('--bg-light1', 'rgb(255, 255, 255');
+    root.style.setProperty('--bg-light2', 'rgb(233, 232, 232)');
+    
+    root.style.setProperty('--icon-color', 'rgb(0, 0, 0)');
+
+    root.style.setProperty('--header-color', 'rgb(0, 0, 0)');
+
+    root.style.setProperty('--footer-color', 'rgb(0, 0, 0)');
+
+    root.style.setProperty('--navbar-bg-color', 'rgb(0, 0, 0)');
+    root.style.setProperty('--navbar-color', 'rgb(124, 120, 120)');
+    root.style.setProperty('--navbar-color-hover', 'rgb(255, 255, 255)');
+  }
 }
-
-
-
 
 var isEnglish = true; // Language variable
-
 // Functions for switching languages
 function changeLanguage() {
     var caption1 = document.getElementById('about');
